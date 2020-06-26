3dc9d00d728566d75377e7c2afcbfc15f1619419
Lucio Nonato <luciononato@hotmail.com>
2020-06-26 18:55:58 -0300

    6 commit
    recebendo dado por id via query.params; configurando rota show instructors

--- routes.js
+++ routes.js
@@ -4,8 +4,9 @@ const routes = express.Router()
 const instructors = require('./instructors')
 // Rotas
 routes.get('/', function(req, res) {
-    return res.redirect("/instructors")
+    return res.redirect('/instructors')
 })
+
 
 routes.get('/instructors', function(req, res) {
     return res.render("instructors/index")
@@ -15,7 +16,9 @@ routes.get('/instructors/create', function(req, res) {
     return res.render('instructors/create')
 })
 
-routes.post("/instructors", instructors.post)
+routes.post('/instructors', instructors.post)
+
+routes.get('/instructors/:id', instructors.show)
 
 routes.get('/members', function(req, res) {
     return res.send("members")
