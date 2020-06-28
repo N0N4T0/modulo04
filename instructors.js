7473b7912e97b04146e3754c90e9d4c78886df12
Lucio Nonato <luciononato@hotmail.com>
2020-06-27 21:13:52 -0300

    13 commit
    uso de método slice() para cortar zero a mais quando aparecer e evitar "012" em mes ou dia.

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
 //edit
 exports.edit = function(req, res){
@@ -12,7 +12,12 @@ exports.edit = function(req, res){
 
     if(!foundInstructor) return res.send("Instructor not found")
 
-    return res.render('instructors/edit', {instructor: foundInstructor})
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', {instructor})
 }
 
 
