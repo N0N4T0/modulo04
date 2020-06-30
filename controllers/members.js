9163aa970db8981a02179fd651117c9d85cd3fcc
Lucio Nonato <luciononato@hotmail.com>
2020-06-29 23:26:57 -0300

    23 commit
    página show dos membros

--- controllers/members.js
+++ controllers/members.js
@@ -14,21 +14,73 @@ exports.create = function(req, res) {
 
 
 //CRUD
-//delete
-exports.delete = function(req, res){
-    const { id } = req.body
+//create 
+exports.post = function(req, res) {
+    //validando dados    
+    const keys = Object.keys(req.body)
+    
+    for(key of keys) {
+        if(req.body[key] == ""){
+            return res.send("Por favor preencha todos os campos")
+        }
+    }
 
-    const filteredMembers = data.members.filter(function(member){
-        return member.id != id
+    birth = Date.parse(req.body.birth)
+
+    let id = 1
+    const lastMember = data.members[data.members.length - 1]
+
+    if(lastMember) {
+        id = lastMember.id + 1
+    } 
+
+    data.members.push({
+        id,
+        ...req.body,
+        birth,
     })
-
-    data.members = filteredMembers
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if(err) return res.send("Write file error")
 
-        return res.redirect('/members')
+        return res.redirect(`/members/${id}`)
     })
+}
+
+//show
+exports.show = function(req, res){
+    const { id } = req.params
+
+    const foundMember = data.members.find(function(member){
+        return member.id == id
+    })
+
+    if(!foundMember) return res.send("Member not found")
+
+    const member = {
+        ...foundMember,
+        age: age(foundMember.birth),
+    }
+
+    return res.render('members/show', { member })
+}
+
+//edit
+exports.edit = function(req, res){
+    const { id } = req.params
+
+    const foundMember = data.members.find(function(member){
+        return member.id == id
+    })
+
+    if(!foundMember) return res.send("Member not found")
+
+    const member = {
+        ...foundMember,
+        birth: date(foundMember.birth)
+    }
+
+    return res.render('members/edit', {member})
 }
 
 //atualizar
@@ -61,73 +113,19 @@ exports.put = function(req, res){
     })
 }
 
-//edit
-exports.edit = function(req, res){
-    const { id } = req.params
+//delete
+exports.delete = function(req, res){
+    const { id } = req.body
 
-    const foundMember = data.members.find(function(member){
-        return member.id == id
+    const filteredMembers = data.members.filter(function(member){
+        return member.id != id
     })
 
-    if(!foundMember) return res.send("Member not found")
-
-    const member = {
-        ...foundMember,
-        birth: date(foundMember.birth)
-    }
-
-    return res.render('members/edit', {member})
-}
-
-
-//show
-exports.show = function(req, res){
-    const { id } = req.params
-
-    const foundMember = data.members.find(function(member){
-        return member.id == id
-    })
-
-    if(!foundMember) return res.send("Member not found")
-
-    const member = {
-        ...foundMember,
-        age: age(foundMember.birth),
-    }
-
-    return res.render('members/show', { member })
-}
-
-
-//create 
-exports.post = function(req, res) {
-    //validando dados    
-    const keys = Object.keys(req.body)
-    
-    for(key of keys) {
-        if(req.body[key] == ""){
-            return res.send("Por favor preencha todos os campos")
-        }
-    }
-
-    birth = Date.parse(req.body.birth)
-
-    let id = 1
-    const lastMember = data.members[data.members.length - 1]
-
-    if(lastMember) {
-        id = lastMember.id + 1
-    } 
-
-    data.members.push({
-        id,
-        ...req.body,
-        birth,
-    })
+    data.members = filteredMembers
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if(err) return res.send("Write file error")
 
-        return res.redirect(`/members/${id}`)
+        return res.redirect('/members')
     })
 }
